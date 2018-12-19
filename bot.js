8e569de542a007457b9318e32826e5c545c83b8a
MadnessDestorys <45974824+MadnessDestorys@users.noreply.github.com>
2018-12-19 14:04:51 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.I7Lq1eHEzoWOUFxXBWkwhbk-_xasP20d);//BOT_TOKEN is the Client Secret
